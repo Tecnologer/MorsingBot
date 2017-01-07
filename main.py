767b871768cc5e35143419a3f5ddf3a0150ed013
Rey David Dominguez Soto <rdominguez@tecnologer.net>
2017-01-07 16:23:59 +0300

    Se elimino el mensaje cuando un comando no es valido, para que pueda convivir con otros bots

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from google.appengine.ext import ndb
 from BotFunctions import *
 import webapp2
 
-TOKEN = '291293792:AAH8WHPfjlrOEdHJPFhiAkFjgkMb4G04Dic'
+TOKEN = 'TOKEN'
 
 BASE_URL = 'https://api.telegram.org/bot' + TOKEN + '/'
 
